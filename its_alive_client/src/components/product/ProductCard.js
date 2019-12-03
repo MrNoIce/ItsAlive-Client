bec705ffaf5762da7c3cc2bf5420eb4c47339a61
Jake Scott <bemybanker@gmail.com>
2019-12-03 14:50:25 -0600

    card is expanding

--- its_alive_client/src/components/product/ProductCard.js
+++ its_alive_client/src/components/product/ProductCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,29 +10,48 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
 
 
-const useStyles = makeStyles({
-    card: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 140,
-    },
-  });
-  const styles =
-  {
 
+
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9,
-    marginTop:'30'
-  }
-    };
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 const Product = props => {
     const { isAuthenticated } = useSimpleAuth()
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
 
 
     const addToOrder = (newOrder) => {
@@ -52,35 +72,64 @@ const Product = props => {
 
     const totalAvailable = props.product.quantity - props.product.total_sold
     return (
-            //   <section className="product">
-        <>
+
         <Card className={classes.card}>
             <CardActionArea>
-            <div key={props.product.id} className="card-body">
-            <CardMedia
-                className={classes.media}
-                image={require("/Users/jakescott/workspace/ItsAlive/ItsAlive-Client/its_alive_client/src/static/images/kombuchaStock.jpg")}
-                title="Kombucha Stock Photo"
-                style={styles.media}
-            />
-              <p className="card-text">${props.product.price}</p>
-              <p className="card-text"><b>{totalAvailable}</b> available</p>
-                <Link className="nav-link" to={`/products/${props.product.id}`}>
-                    <h5>{props.product.name}</h5>
-                </Link>
-                {isAuthenticated() ?
-                <Button onClick={() => addToOrder(props.product)}
-                variant="contained" color="primary">
-                    Get it
-                </Button>
-                : null
-            }
-
-            </div>
+                <div key={props.product.id} className="card-body">
+                    <CardMedia
+                        className={classes.media}
+                        image={require("/Users/jakescott/workspace/ItsAlive/ItsAlive-Client/its_alive_client/src/static/images/kombuchaStock.jpg")}
+                        title="Kombucha Stock Photo"
+                        style={useStyles.media}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            <Link className="nav-link" to={`/products/${props.product.id}`}>
+                                <h5>{props.product.name}</h5>
+                            </Link>
+                        </Typography>
+                        <p className="card-text">${props.product.price}</p>
+                    </CardContent>
+                    <CardActions>
+                        {isAuthenticated() ?
+                        <Button size='small' onClick={() => addToOrder(props.product)}
+                         color="primary">
+                            Get it
+                        </Button>
+                        : null
+                    }
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                        >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>
+                                {props.product.description}
+                                This kombooboo be hand crafted for sure
+                            </Typography>
+                            <Typography paragraph>
+                                Ol Dudley makes this junk and its poppin. Its the best dang kombucha i done ever ever had
+                            </Typography>
+                            <Typography paragraph>
+                                One drank and youll be hooked, its the truth for real
+                            </Typography>
+                            <Typography>
+                                gone head and cop you some of this kombooboo
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </div>
             </CardActionArea>
         </Card>
-        </>
-            // </section>
+
     )
 }
 
